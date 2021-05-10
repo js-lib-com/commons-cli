0c695362f1ba237441abec8182a4576c95df4284
Iulian Rotaru <iulian@gnotis.ro>
2021-05-10 16:01:12 +0300

    Use velocity engine for template processor.

--- src/main/java/com/jslib/commons/cli/TemplateProcessor.java
+++ src/main/java/com/jslib/commons/cli/TemplateProcessor.java
@@ -5,7 +5,6 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,7 +15,8 @@ import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import js.io.VariablesWriter;
+import org.apache.velocity.VelocityContext;
+
 import js.util.Strings;
 
 public class TemplateProcessor
@@ -36,7 +36,10 @@ public class TemplateProcessor
 
   public void exec(String type, String templateName, Map<String, String> variables) throws IOException
   {
-    try (ZipInputStream zipInputStream = template(type, templateName)) {
+    File woodHomeDir = new File(Home.getPath());
+    File templateFile = new File(woodHomeDir, Strings.concat("template", File.separatorChar, type, File.separatorChar, templateName, ".zip"));
+
+    try (ZipInputStream zipInputStream = new ZipInputStream(new BufferedInputStream(new FileInputStream(templateFile)))) {
       ZipEntry zipEntry;
       while((zipEntry = zipInputStream.getNextEntry()) != null) {
         String zipEntryName = Strings.injectVariables(zipEntry.getName(), variables);
@@ -48,9 +51,9 @@ public class TemplateProcessor
 
         String[] zipEntryNameSegments = zipEntryName.split("/");
         String fileName = zipEntryNameSegments[zipEntryNameSegments.length - 1];
-        // by convention, for formatted files, file name starts with dollar ($)
-        if(fileName.startsWith("!")) {
-          zipEntryNameSegments[zipEntryNameSegments.length - 1] = fileName.substring(1);
+        // by convention, for formatted files, file name has .vtl extension
+        if(fileName.endsWith(".vtl")) {
+          zipEntryNameSegments[zipEntryNameSegments.length - 1] = fileName.substring(0, fileName.length() - 4);
           copy(zipInputStream, Strings.join(zipEntryNameSegments, '/'), variables);
         }
         else {
@@ -73,31 +76,30 @@ public class TemplateProcessor
 
   private void copy(ZipInputStream zipInputStream, String zipEntryName, Map<String, String> variables) throws IOException
   {
-    char[] buffer = new char[2048];
-
     File file = new File(targetDir, zipEntryName);
     if(verbose) {
       print("Create file '%s'.", file);
     }
 
-    Reader reader = new InputStreamReader(zipInputStream);
-    try (Writer writer = new VariablesWriter(new BufferedWriter(new FileWriter(file)), variables)) {
-      int len;
-      while((len = reader.read(buffer)) > 0) {
-        writer.write(buffer, 0, len);
-      }
+    VelocityContext context = new VelocityContext();
+    for(Map.Entry<String, String> entry : variables.entrySet()) {
+      context.put(entry.getKey(), entry.getValue());
+    }
+
+    Reader reader = new UncloseableReader(new InputStreamReader(zipInputStream));
+    try (Writer writer = new BufferedWriter(new FileWriter(file))) {
+      org.apache.velocity.app.Velocity.evaluate(context, writer, zipEntryName, reader);
     }
   }
 
   private void copy(ZipInputStream zipInputStream, String zipEntryName) throws IOException
   {
-    byte[] buffer = new byte[2048];
-
     File file = new File(targetDir, zipEntryName);
     if(verbose) {
       print("Create file '%s'.", file);
     }
 
+    byte[] buffer = new byte[2048];
     try (BufferedOutputStream fileOutputStream = new BufferedOutputStream(new FileOutputStream(file), buffer.length)) {
       int len;
       while((len = zipInputStream.read(buffer)) > 0) {
@@ -106,16 +108,31 @@ public class TemplateProcessor
     }
   }
 
-  private static ZipInputStream template(String type, String name) throws FileNotFoundException
-  {
-    File woodHomeDir = new File(Home.getPath());
-    File templateFile = new File(woodHomeDir, Strings.concat("template", File.separatorChar, type, File.separatorChar, name, ".zip"));
-    return new ZipInputStream(new BufferedInputStream(new FileInputStream(templateFile)));
-  }
-
   protected static void print(String format, Object... args)
   {
     System.out.printf(format, args);
     System.out.println();
+  }
+
+  private static class UncloseableReader extends Reader
+  {
+    private final Reader reader;
+
+    public UncloseableReader(Reader reader)
+    {
+      super();
+      this.reader = reader;
+    }
+
+    @Override
+    public int read(char[] cbuf, int off, int len) throws IOException
+    {
+      return reader.read(cbuf, off, len);
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+    }
   }
 }
