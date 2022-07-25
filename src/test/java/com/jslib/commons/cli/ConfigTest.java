e2d9fd8f4f25da49cce8746ac7914996890d1950
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2022-07-25 04:15:53 +0300

    Update to Java 11 and Jakarta EE.

--- src/test/java/com/jslib/commons/cli/ConfigTest.java
+++ src/test/java/com/jslib/commons/cli/ConfigTest.java
@@ -11,7 +11,6 @@ import java.util.Properties;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
